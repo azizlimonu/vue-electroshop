101d5dbbb743edc58aaa554287239b0560cc1c7f
Aziz Limonu <azizlimonu@gmail.com>
2023-12-14 21:52:45 +0800

    fix sidebar cart

--- src/components/Navbar/middle/NavLinks.vue
+++ src/components/Navbar/middle/NavLinks.vue
@@ -1,11 +1,611 @@
 <template>
-  <div>
-    Links
+  <div class="links">
+
+    <div class="search" @click="toggleSearch" ref="search">
+      <i class="fa-solid fa-magnifying-glass open-search"></i>
+      <span class="open-search">Search</span>
+    </div>
+
+    <div class="user">
+      <router-link to="/profile-page">
+        <i class="fa-regular fa-user"></i>
+      </router-link>
+      <span>Account</span>
+    </div>
+
+    <div class="fav">
+      <router-link to="/wishlist">
+        <i class="fa-regular fa-heart">
+          <span>
+            {{ store.state.favTotal }}
+          </span>
+        </i>
+      </router-link>
+      <span>Wishlist</span>
+    </div>
+
+    <div class="compare">
+      <router-link to="/compare">
+        <i class="fa-solid fa-arrow-rotate-left">
+          <span class="compare-num">
+            {{ store.state.compareTotal }}
+          </span>
+        </i>
+      </router-link>
+      <span>Compare</span>
+    </div>
+
+    <div class="cart" @click="location.path === '/my-cart' ? false : toggleCart()" ref="myCart">
+      <i class="fa-solid fa-cart-shopping">
+        <span class="cart-num purchase-num">
+          {{ store.state.cartTotal }}
+        </span>
+      </i>
+      <span>My Cart</span>
+    </div>
+
+    <!-- Side Cart When Its Toggled -->
+    <div class="home-cart">
+      <div :class="['overlay', isModalVisible ? 'show' : '']" @click="toggleCart"></div>
+
+      <div :class="['purchase', 'd-flex', 'flex-column', isModalVisible ? 'show' : '']">
+        Alhamdulillah Ya Rabb
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, computed, onMounted, onBeforeUnmount, nextTick } from 'vue';
+import { useStore } from 'vuex';
+import { useRoute } from 'vue-router'
+const location = useRoute();
 
+const isCartOpen = ref(false);
+const isSearchOpen = ref(false);
+const homeCart = ref([]);
+const store = useStore();
+const windowWidth = ref(window.innerWidth);
+const isModalVisible = ref(false);
+
+console.log("State => ", store.state);
+
+// Custom nextTick function to capture refs
+const refs = ref({});
+console.log(refs);
+const customNextTick = (callback) => {
+  refs.value = {
+    ...refs.value,
+    ...refs,
+  };
+  return callback();
+};
+
+// Compute the total price based on the homeCart
+const totalPrice = computed(() => {
+  return `Rp. ${homeCart.value
+    .reduce(
+      (total, product) =>
+        total +
+        Math.floor(
+          product.price - (product.price * product.discount) / 100
+        ) *
+        product.count,
+      0
+    )
+    .toFixed(2)}`;
+});
+
+// Event handler for window resize
+const handleResize = () => {
+  // handle resize logic
+  windowWidth.value = window.innerWidth;
+};
+
+// Toggle cart visibility
+const toggleCart = () => {
+  homeCart.value = store.state.cart;
+  isModalVisible.value = !isModalVisible.value
+};
+
+// Toggle search visibility
+const toggleSearch = () => {
+  isSearchOpen.value = !isSearchOpen.value;
+};
+
+// Save cart to local storage
+const setCartCountToLS = () => {
+  localStorage.setItem('cart', JSON.stringify(homeCart.value));
+};
+
+// Delete an item from the cart
+const deleteItem = (i) => {
+  if (window.confirm('Are you sure you want to delete this product?')) {
+    homeCart.value.splice(i, 1);
+    setCartCountToLS();
+    store.commit('totalCart');
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize);
+});
 </script>
 
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.links {
+  display: flex;
+  justify-content: end;
+  color: white;
+
+  a:hover {
+    color: white;
+  }
+
+  .router-link-exact-active {
+    color: var(--yellow);
+
+    +span {
+      color: var(--yellow);
+    }
+  }
+
+  >div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 10px;
+
+    i {
+      font-size: 25px;
+      position: relative;
+
+      span {
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(50%, -50%);
+        font-size: 10px;
+        width: 18px;
+        height: 18px;
+        z-index: 3;
+        color: black;
+        background-color: var(--yellow);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        font-weight: bold;
+      }
+    }
+
+    &.cart {
+      cursor: pointer;
+
+      @media (max-width: 1199px) {
+        padding-right: 0;
+      }
+
+      i {
+        margin-bottom: 3px;
+        pointer-events: none;
+      }
+    }
+
+    @media (max-width: 767px) {
+      & {
+        padding: 0 7px;
+
+        >span {
+          display: none;
+        }
+
+        i {
+          font-size: 22px;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 420px) {
+    & {
+      .fav {
+        display: none;
+      }
+
+      .compare {
+        display: none;
+      }
+
+      >div {
+        padding: 0 7px;
+
+        i {
+          font-size: 22px;
+        }
+      }
+    }
+  }
+
+  .home-cart {
+    padding: 0;
+
+    .overlay {
+      position: relative;
+      top: 0;
+      left: 0;
+      background: rgba(0, 0, 0, 0.603);
+      z-index: -1;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      transition: 0.3s;
+
+      &.show {
+        position: fixed;
+        opacity: 1;
+        z-index: 999999999999;
+      }
+    }
+
+    .purchase {
+      position: fixed;
+      background-color: white;
+      height: 100%;
+      z-index: 99999999999;
+      right: 0;
+      top: 0;
+      color: var(--bg-color);
+      width: 400px;
+      transition: 0.3s ease-in-out;
+      transform: translateX(103%);
+
+      @media (max-width: 767px) {
+        & {
+          width: 60%;
+        }
+      }
+
+      &.show {
+        transform: translateX(0);
+      }
+
+      .heading {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: var(--bg-color);
+        padding: 15px;
+        border-bottom: 1px solid var(--yellow);
+
+        .home-cart-img {
+          width: 160px;
+
+          img {
+            width: 100%;
+          }
+        }
+
+        .close-cart {
+          cursor: pointer;
+          color: white;
+          display: flex;
+
+          i {
+            font-size: 22px;
+          }
+        }
+
+        @media (max-width: 767px) {
+          & {
+            padding: 15px;
+
+            .home-cart-img {
+              width: 165px;
+            }
+
+            .close-cart {
+              i {
+                font-size: 23px;
+              }
+            }
+          }
+        }
+      }
+
+      .products {
+        position: relative;
+        padding-top: 10px;
+        flex: 1;
+        overflow-y: scroll;
+
+        &::-webkit-scrollbar {
+          width: 7px;
+        }
+
+        &::-webkit-scrollbar-track {
+          background-color: transparent;
+        }
+
+        &::-webkit-scrollbar-thumb {
+          background-color: rgba(128, 128, 128, 0.404);
+          transition: 0.3s;
+          border-radius: 4px;
+        }
+
+        &::-webkit-scrollbar-thumb:hover {
+          background-color: rgba(128, 128, 128);
+        }
+
+        &::-webkit-scrollbar-corner {
+          background-color: white;
+          box-shadow: 0 0 10px #ddd inset;
+        }
+
+        .item {
+          display: flex;
+          padding: 10px 0;
+
+          .product-img {
+            width: 150px;
+
+            a {
+              cursor: pointer;
+            }
+
+            img {
+              width: 150px;
+            }
+          }
+
+          .product-text {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+
+            a {
+              cursor: pointer;
+              font-weight: 500;
+              color: var(--bg-color);
+              font-size: 16px;
+            }
+
+            p {
+              font-weight: 500;
+              font-size: 15px;
+              margin: 0;
+              margin-top: 10px;
+            }
+          }
+
+          @media (max-width: 767px) {
+            & {
+              .product-img {
+                width: 90px !important;
+
+                img {
+                  width: 90px !important;
+                }
+              }
+
+              .product-text {
+                .link {
+                  font-size: 14px !important;
+                }
+
+                p {
+                  font-size: 14px !important;
+                }
+              }
+            }
+          }
+
+          .delete {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            button {
+              border: none;
+              background-color: transparent;
+              margin-right: 10px;
+
+              i {
+                pointer-events: none;
+                font-size: 17px;
+              }
+            }
+          }
+        }
+      }
+
+      @media (max-width: 576px) {
+        & {
+          .heading {
+            .home-cart-img {
+              width: 135px !important;
+            }
+
+            .close-cart {
+              i {
+                font-size: 20px;
+              }
+            }
+          }
+        }
+      }
+
+      @media (max-width: 420px) {
+        & {
+          width: 75%;
+
+          .heading {
+            .home-cart-img {
+              width: 135px !important;
+            }
+
+            .close-cart {
+              i {
+                font-size: 20px;
+              }
+            }
+          }
+        }
+      }
+
+      .empty-cart {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        flex: 1;
+
+        h2 {
+          font-size: 23px;
+          font-weight: 400;
+        }
+
+        .continue-link {
+          a {
+            color: var(--bg-color);
+            font-size: 16px;
+            text-decoration: underline;
+          }
+
+          &:hover {
+            a {
+              color: var(--yellow);
+              text-decoration: underline;
+            }
+          }
+        }
+      }
+
+      .cart-footer {
+        width: 100%;
+        padding: 15px;
+        gap: 15px;
+        background-color: var(--bg-color);
+        color: white;
+        z-index: 9;
+        border-top: 1px solid var(--yellow);
+
+        >div {
+          margin-bottom: 20px;
+        }
+
+        .total {
+          font-size: 17px;
+
+          .total-num {
+            font-weight: 500;
+          }
+        }
+
+        .view-cart,
+        .pay {
+          display: block;
+          padding: 15px 30px;
+          text-align: center;
+          border-radius: 6px;
+          font-weight: 500;
+          letter-spacing: 0.6px;
+          width: 100%;
+          color: var(--bg-color);
+          background-color: var(--yellow);
+          font-weight: 500;
+
+          &.router-link-active.router-link-exact-active {
+            color: var(--bg-color) !important;
+          }
+
+          &.router-link-active.router-link-exact-active {
+            color: var(--bg-color) !important;
+          }
+        }
+      }
+    }
+  }
+
+  .search {
+    cursor: pointer;
+
+    span {
+      margin-top: 3px;
+    }
+
+    @media (min-width: 1200px) {
+      & {
+        display: none;
+      }
+    }
+  }
+
+  .hidden-search {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #2e3f54;
+    color: var(--bg-color);
+    opacity: 0;
+    z-index: -3;
+    transition: 0.3s;
+
+    &.show-search {
+      opacity: 1;
+      z-index: 899879;
+    }
+
+    .container {
+      width: 100%;
+      height: 100%;
+
+      .search-content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        form {
+          width: 70%;
+          margin-right: 15px;
+
+          .form-floating {
+            .form-control {
+              height: 44px;
+
+              &:focus {
+                border-color: #ced4da;
+                box-shadow: none;
+              }
+            }
+
+            label {
+              color: grey;
+              padding: 10px;
+            }
+          }
+        }
+
+        .close-search {
+          font-size: 24px;
+          color: white;
+          border: none;
+          background-color: transparent;
+          padding: 0;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/Navbar/middle/NavSearch.vue
+++ src/components/Navbar/middle/NavSearch.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    Search
+  <div class="search-input">
+    <input type="text" placeholder="Search Here..." />
+    <button>Search</button>
   </div>
 </template>
 
@@ -8,4 +9,37 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search-input {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+
+  input {
+    padding: 8px;
+    outline: none;
+    border-radius: 6px;
+    border: 1px solid var(--yellow);
+    width: 70%;
+
+    &::placeholder {
+      font-size: small;
+    }
+  }
+
+  button {
+    padding: 5px 15px;
+    border: 1px solid var(--yellow);
+    border-radius: 6px;
+    background-color: var(--yellow);
+  }
+
+  @media (min-width: 1199px) {
+    flex: 1;
+  }
+
+  @media (max-width: 1199px) {
+    display: none;
+  }
+}
+</style>
