fa1c6cc250d57e0d4191d4b887c56effd8a70247
Aziz Limonu <azizlimonu@gmail.com>
2023-12-18 09:25:35 +0800

    add cart page & cart content

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>Cart</div>
+  <div class="my-cart">
+    <CartContent />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import CartContent from '../components/Cart/CartContent.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.my-cart {
+  flex: 1;
+  @media (max-width: 767px) {
+    max-width: 100%;
+  }
+  @media (max-width: 1199px) {
+    flex-direction: column-reverse;
+    padding: 0 12px;
+  }
+}
+</style>
